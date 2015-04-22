a815c60316ebdddd1cbe15e796b48ae96345bcef
Ovi-Wan Kenobi <ovidiuc@gmail.com>
2015-04-22 12:28:13 +0200

    test checkin: more comments

--- AngularJSTodoListWebsite/app/app.js
+++ AngularJSTodoListWebsite/app/app.js
@@ -3,9 +3,11 @@
 var app = angular.module('AngularJSTodoListListApp', ['ngRoute']);
 
 // using of constant service; the author suppose to not change, be constant :)
+// this can be used just like value service + in app.config
 app.constant('author', 'Ovi-Wan Kenobi');
 
 // using the value service; the version would eventually change, therefor not a constant.
+// value service can not be used in app.config as the constant service can
 app.value('version', '1.0');
 
 
